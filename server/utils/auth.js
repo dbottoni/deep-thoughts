e52c13545cdd2cbcd25b15565561289444d0183e
Dana Bottoni <danabottoni@gmail.com>
2021-10-04 17:51:39 -0400

    finish mutations

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -8,7 +8,7 @@ module.exports = {
     // allows token to be sent via req.body, req.query, or headers
     let token = req.body.token || req.query.token || req.headers.authorization;
 
-    // ["Bearer", "<tokenvalue>"]
+    // separate "Bearer" from "<tokenvalue>"
     if (req.headers.authorization) {
       token = token
         .split(' ')
@@ -16,11 +16,13 @@ module.exports = {
         .trim();
     }
 
+    // if no token, return request object as is
     if (!token) {
       return req;
     }
 
     try {
+      // decode and attach user data to request object
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
     } catch {
@@ -29,6 +31,7 @@ module.exports = {
 
     return req;
   },
+  
   signToken: function({ username, email, _id }) {
     const payload = { username, email, _id };
 
