9b144f9776fcb2ac39616a27922b0652944e4bac
Dana Bottoni <danabottoni@gmail.com>
2021-10-04 17:29:29 -0400

    mutations and auth

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,8 +1,9 @@
+const { AuthenticationError } = require('apollo-server-express');
 const { User, Thought } = require('../models');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
-
     // get all users
     users: async () => {
       return User.find()
@@ -23,6 +24,32 @@ const resolvers = {
     },
     thought: async (parent, { _id }) => {
       return Thought.findOne({ _id });
+    }
+  },
+
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+      return { token, user };
     }
   }
 };
